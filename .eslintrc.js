41ad53e9ead5fc423bfcddbd69ae2d74ad8deeaa
mayunited12 <mayunited12@yahoo.com>
2020-06-29 21:11:19 +0100

    Attempt to Get rid of all console.logs(ES LINT Disable)]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
   parserOptions: {
-    parser: "babel-eslint"
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    browser: true
+  },
+  rules: {
+    "no-console": 0
   }
-};
+}
